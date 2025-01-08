3b1748463913e5bc0ee5ff6fe756f3f3edcf0472
Shin Umeda <umeda.shin@gmail.com>
2025-01-07 16:16:44 -0800

    parallelized download process, added darwin and use local docker image

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,11 @@
-FROM ubuntu:jammy
+FROM crossbuild:latest
 # modified from https://github.com/rleh/docker-avr-gcc/tree/master
 LABEL maintainer="Shin Umeda <umeda.shin@gmail.com>"
 LABEL Description="Image for building AVR GCC toolchain"
 WORKDIR /work
 
-ENV DEBIAN_FRONTEND=noninteractive
+COPY avr.sh avr.sh
+COPY work.sh work.sh
+COPY patches patches
+COPY apply-paches.sh apply-patches.sh
 
-RUN apt-get update \
-    && apt-get upgrade -y
-RUN apt-get install -y   \
-    gcc-12   \
-    mingw-w64   \
-    git    \
-    wget    \
-    atool        \
-    autoconf2.64         \
-    g++     \
-    libc6-dev \
-    make         \
-    libmpc-dev   \
-    libmpfr-dev  \
-    libgmp-dev \
-    libmpfr6     \
-    texinfo       \
-    flex \
-    bison \
-    zstd \
-    parallel
-
-RUN   apt-get clean \
-       && rm -rf /var/lib/apt/lists/*
-
-ADD avr.sh avr.sh
-ADD work.sh work.sh
-ADD patches patches
-ADD apply-paches.sh apply-patches.sh
--- work.sh
+++ work.sh
@@ -8,7 +8,6 @@ links=(
     https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.xz
     https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
     https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-13.2.0/gcc-13.2.0.tar.xz
-    https://github.com/DolphinGui/osxcross/releases/download/v1/target.tar.xz
 )
 
 names=(
@@ -18,35 +17,32 @@ names=(
     mpfr
     mpc
     gcc
-    osxtoolchain
 )
 
-
-parallel --plus --link "wget {1} && aunpack {1/} && if [ ! {1/..} = {2} ]; then mv {1/..} {2}; fi" ::: ${links[@]} ::: ${names[@]}
-
-export PATH=$(pwd)/osxtoolchain/bin/:"$PATH"
-
-ls $(pwd)/osxtoolchain
-
-o64-clang --version
+parallel --link \
+  'TMP=$(mktemp /tmp/aunpack.XXXXXXXXXX) \
+  && wget {1} \
+  && aunpack --save-outdir=$TMP {1/} && DIR=$(cat $TMP) \
+  && if [ ! "$DIR" = {2} ]; then mv $DIR {2}; fi; rm $TMP' \
+  ::: ${links[@]} ::: ${names[@]}
 
 sh apply-patches.sh
 
 export HOST="x86_64-pc-linux-gnu"
+export CROSS_TRIPLE=x86_64-linux-gnu
 
 sh avr.sh /out/root
 
-export HOST="arm64-apple-darwin23.5"
+export PATH=/out/root/bin:$PATH
+
+export HOST="x86_64-apple-darwin"
 export HOSTFLAG="--host=$HOST"
-export CC=arm64-apple-darwin23.5-clang
-export CXX=arm64-apple-darwin23.5-clang-g++
+export CROSS_TRIPLE=x86_64-apple-darwin
 
 sh avr.sh /out/osxroot
 
-export PATH=/out/root/bin:$PATH
 export HOST="x86_64-w64-mingw32"
 export HOSTFLAG="--host=$HOST"
-export CC=x86_64-w64-mingw32-gcc
-export CXX=x86_64-w64-mingw32-g++
+export CROSS_TRIPLE=x86_64-w64-mingw32
 
 sh avr.sh /out/winroot
