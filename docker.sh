0317ac4076276f72b6cf32c34e505c7bde0fa213
Shin Umeda <umeda.shin@gmail.com>
2025-06-28 21:41:59 -0700

    switched back to xz bc of better compression

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ podman build -t avr-build .
 mkdir -p out
 podman run --mount type=bind,src="$(pwd)"/out,dst=/out --mount type=bind,src="$(pwd)"/cache,dst=/work/cache  avr-build bash work.sh
 cd out
-tar --use-compress-program="zstd -T0" -cf root.tar.zst root/
-tar --use-compress-program="zstd -T0" -cf winroot.tar.zst winroot/
-tar --use-compress-program="zstd -T0" -cf osxroot.tar.zst osxroot/
+tar --use-compress-program="pixz -8" -cf root.tar.xz root/
+tar --use-compress-program="pixz -8" -cf winroot.tar.xz winroot/
+tar --use-compress-program="pixz -8" -cf osxroot.tar.xz osxroot/
 cd ..
